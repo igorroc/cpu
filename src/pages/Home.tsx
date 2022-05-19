5f1e9c44499a87c9ac3c42bf1ea850595104f544
Igor Rocha <igor_roc@hotmail.com.br>
2022-05-19 08:17:08 -0300

    [add]: Regulamento

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -79,6 +79,14 @@ const Section = styled("div", {
 					overflow: "hidden",
 					transition: "all 300ms ease",
 					background: "#f7f7f7",
+
+					"& ol": {
+						paddingLeft: "1em",
+					},
+
+					"& > ol > li > ol > li": {
+						listStyleType: "lower-alpha",
+					},
 				},
 
 				"&:hover .rowContent": {
@@ -133,10 +141,19 @@ function Home() {
 								<h4>Modelos de Equipe</h4>
 							</div>
 							<div className="rowContent">
-								As equipes deverão estar cadastradas pelo Form,
-								sem a necessidade de pagamento para o CPU
-								COMPETITIVO Equipes participantes devem ser
-								constituídas obrigatoriamente por 4 integrantes
+								<p>
+									As equipes deverão estar cadastradas pelo
+									Form, sem a necessidade de pagamento para o
+									CPU
+								</p>
+								<p>
+									Equipes competitivas devem ser constituídas
+									obrigatoriamente por 4 integrantes
+								</p>
+								<p>
+									Equipes café com leite devem ser
+									constituídas por até 3 integrantes
+								</p>
 							</div>
 						</div>
 						<div className="row">
@@ -145,46 +162,138 @@ function Home() {
 								<h4>Equipamentos</h4>
 							</div>
 							<div className="rowContent">
-								As equipes deverão estar cadastradas pelo Form,
-								sem a necessidade de pagamento para o CPU
-								COMPETITIVO Equipes participantes devem ser
-								constituídas obrigatoriamente por 4 integrantes
+								Vai ser disponibilizado uma máquina para cada
+								equipe
 							</div>
 						</div>
 						<div className="row">
 							<div className="title">
 								<span className="number">III</span>
-								<h4>Linguagens</h4>
+								<h4>Aquecimento</h4>
 							</div>
 							<div className="rowContent">
-								As equipes deverão estar cadastradas pelo Form,
-								sem a necessidade de pagamento para o CPU
-								COMPETITIVO Equipes participantes devem ser
-								constituídas obrigatoriamente por 4 integrantes
+								Acontecerá das 12:00 às 14:00
 							</div>
 						</div>
 						<div className="row">
 							<div className="title">
 								<span className="number">IV</span>
-								<h4>Material de apoio</h4>
+								<h4>Sobre a Entrada</h4>
 							</div>
 							<div className="rowContent">
-								As equipes deverão estar cadastradas pelo Form,
-								sem a necessidade de pagamento para o CPU
-								COMPETITIVO Equipes participantes devem ser
-								constituídas obrigatoriamente por 4 integrantes
+								<ol>
+									<li>
+										A entrada de seu programa deve ser lida
+										da entrada padrão.
+									</li>
+									<li>
+										A entrada é composta por vários casos de
+										teste, cada um descrito em um número de
+										linhas que depende do problema.
+									</li>
+									<li>
+										Quando uma linha da entrada contém
+										vários valores, estes são separados por
+										um único espaço em branco; a entrada não
+										contém nenhum outro espaço em branco.
+									</li>
+									<li>
+										Cada linha, incluindo a última, contém o
+										caractere final-de-linha.
+									</li>
+									<li>
+										O final da entrada coincide com o final
+										do arquivo.
+									</li>
+								</ol>
 							</div>
 						</div>
 						<div className="row">
 							<div className="title">
 								<span className="number">V</span>
-								<h4>Aquecimento</h4>
+								<h4>Sobre o seu Código</h4>
 							</div>
 							<div className="rowContent">
-								As equipes deverão estar cadastradas pelo Form,
-								sem a necessidade de pagamento para o CPU
-								COMPETITIVO Equipes participantes devem ser
-								constituídas obrigatoriamente por 4 integrantes
+								<ol>
+									<li>
+										O nome do arquivo de seu programa é o
+										nome do problema seguido da extensão da
+										linguagem escolhida.
+									</li>
+									<li>
+										O seu programa deve ser escrito em uma
+										das linguagens permitidas: Java, C, C++
+										ou Python 2 ou 3.
+									</li>
+									<li>
+										Um problema pode ser resolvido em uma
+										linguagem diferente de outro, se
+										desejar.
+									</li>
+									<li>
+										O seu programa deve ler a entrada do
+										console (entrada padrão).
+									</li>
+									<li>
+										O seu programa deve gerar a saı́da em
+										console (saı́da padrão).
+									</li>
+								</ol>
+							</div>
+						</div>
+						<div className="row">
+							<div className="title">
+								<span className="number">VI</span>
+								<h4>Sobre a Saida</h4>
+							</div>
+							<div className="rowContent">
+								<ol>
+									<li>
+										Quando uma linha da saı́da contém vários
+										valores, estes devem ser separados por
+										um único espaço em branco; a saı́da não
+										deve conter nenhum outro espaço em
+										branco.
+									</li>
+									<li>
+										Cada linha, incluindo a última, deve
+										conter o caractere final-de-linha.
+									</li>
+								</ol>
+							</div>
+						</div>
+						<div className="row">
+							<div className="title">
+								<span className="number">VII</span>
+								<h4>Sobre a Competição</h4>
+							</div>
+							<div className="rowContent">
+								<ol>
+									<li>
+										Linguagens aceitas:
+										<ol>
+											<li>
+												C, extensão “.c” → gcc -O2 -lm
+											</li>
+											<li>
+												C++, extensão “.cpp” → g++
+												-std=c++11 -O2 -lm
+											</li>
+											<li>
+												Java, extensão “.java” → (LC
+												ALL=en US.UTF-8 java ..)
+											</li>
+											<li>Python 2, extensão “.py2”</li>
+											<li>Python 3, extensão “.py3”</li>
+										</ol>
+									</li>
+									<li>
+										As questões, embora escritas de forma
+										original pela equipe organizadora, podem
+										ser baseadas em problemas de mesma
+										categoria já existentes.{" "}
+									</li>
+								</ol>
 							</div>
 						</div>
 					</div>
